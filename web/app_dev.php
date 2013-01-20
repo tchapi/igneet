679da52451031e6f183b282f9269a1aa3e6ab02b
tchapi <regbasket@gmail.com>
2013-01-20 14:33:19 +0100

    Add local IP for development purposes

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,6 +12,11 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
+        '109.190.90.220',
+<<<<<<< HEAD
+=======
+        '37.59.11.115',
+>>>>>>> master
         '::1',
     ))
 ) {
