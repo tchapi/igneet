fec3c273789aeb11f1dfced27a5a9019811fb438
tchapi <regbasket@gmail.com>
2013-01-20 15:35:47 +0100

    Add Doctrine Fixtures Create fixtures for Skill in User bundle

---
+++ src/meta/UserProfileBundle/DataFixtures/ORM/LoadSkillData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Acme\UserProfileBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Acme\UserProfileBundle\Entity\User;
+
+class LoadSkillData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $skill = new Skill();
+        $skill->setSlug('development');
+        $skill->setName('Development');
+        $skill->setDescription('Web / Application development');
+        $skill->setColor('00CCBB');
+
+        $manager->persist($skill);
+        $manager->flush();
+
+        $skill = new Skill();
+        $skill->setSlug('commercial');
+        $skill->setName('Commercial');
+        $skill->setDescription('Selling Stuff');
+        $skill->setColor('00CCBB');
+
+        $manager->persist($skill);
+        $manager->flush();
+
+        $skill = new Skill();
+        $skill->setSlug('business-dev');
+        $skill->setName('Business Dev');
+        $skill->setDescription('Developing the business');
+        $skill->setColor('00CCBB');
+
+        $manager->persist($skill);
+        $manager->flush();
+
+        $skill = new Skill();
+        $skill->setSlug('management');
+        $skill->setName('Management');
+        $skill->setDescription('Managing people');
+        $skill->setColor('00CCBB');
+
+        $manager->persist($skill);
+        $manager->flush();
+
+        $skill = new Skill();
+        $skill->setSlug('tech-consulting');
+        $skill->setName('Tech Consulting');
+        $skill->setDescription('Consulting & Technical expertise');
+        $skill->setColor('00CCBB')
+
+        $manager->persist($skill);
+        $manager->flush();
+    }
+}
