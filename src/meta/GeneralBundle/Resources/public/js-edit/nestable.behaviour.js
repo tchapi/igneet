ac82036bf86f11c645001b8a0a940b74e5cad188
tchapi <regbasket@gmail.com>
2013-03-13 15:43:12 +0100

    Replace jQuery sortable with Nestable

---
+++ src/meta/GeneralBundle/Resources/public/js-edit/nestable.behaviour.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+  var sortableList = $("ul.sortable");
+
+  // Callback for ranks and nesting
+  var updateRanksAndNesting = function(l, e) {
+    
+      var list = l.length ? l : $(l.target),
+          item = e.length ? e : $(e.target);
+
+      // Compute ranks
+      var ranks = list.find('li').map(function() {
+                    return this.id;
+                  }).get().join();
+
+      // Update rank
+      $.post(list.attr('data-url'), {
+        ranks: ranks
+      })
+      .error(function(errors) {
+        setFlash('error', 'There was an error saving changes.');
+      });
+
+      // Update parenting
+      $.post(item.attr('data-url'), {
+        name: item.attr('data-name'),
+        value: item.parent().attr('data-value')
+      })
+      .error(function(errors) {
+        setFlash('error', 'There was an error saving changes.');
+      });
+
+  };
+
+  // Add expand / collapse behaviour
+  /*
+  <menu id="nestable-menu">
+      <button type="button" data-action="expand-all">Expand All</button>
+      <button type="button" data-action="collapse-all">Collapse All</button>
+  </menu>
+  $('#nestable-menu').on('click', function(e) {
+      var target = $(e.target),
+          action = target.data('action');
+      if (action === 'expand-all') {
+          $('.dd').nestable('expandAll');
+      }
+      if (action === 'collapse-all') {
+          $('.dd').nestable('collapseAll');
+      }
+  });
+  */
+
+  // Triggers nestable()
+  sortableList.nestable({
+    callback : updateRanksAndNesting
+  });
+
+});
