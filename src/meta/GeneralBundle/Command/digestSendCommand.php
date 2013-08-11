2dc3d76135514235d4c5c50de51586217d46edad
tchapi <regbasket@gmail.com>
2013-08-11 12:34:57 +0200

    Add HTML digest email Fix request context for CLI Activates mail sending

--- src/meta/GeneralBundle/Command/digestSendCommand.php
+++ src/meta/GeneralBundle/Command/digestSendCommand.php
@@ -20,6 +20,7 @@ class digestSendCommand extends ContainerAwareCommand
   
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
       // Should we really send mails
       $sendMails = $input->getOption('force');
       $verbose = (OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity());
@@ -28,26 +29,26 @@ class digestSendCommand extends ContainerAwareCommand
       $sendBiMonthlyEmails = $this->isEvenWeek();
       $sendDefaultDayEmails = $this->isDefaultDay();
 
-      $output->writeln('Today is : ' . $today.'.');
+      $output->writeln('Today is : <info>' . $today.'.</info>');
 
       if ($sendBiMonthlyEmails){
-        $output->writeln(' # This week we are sending bi-monthly emails.');
+        $output->writeln(' # This week we are sending <info>bi-monthly emails</info>.');
       }
 
       if ($sendDefaultDayEmails){
-        $output->writeln(" # It's the default day");
+        $output->writeln(" # It's the <info>default</info> day");
       }
 
       // List all users to whom we need to send a digest today
       $userRepository = $this->getContainer()->get('doctrine')->getRepository('metaUserBundle:User');
       $usersToSendDigestsTo = $userRepository->findUsersWhoNeedDigestOnDay($today, $sendBiMonthlyEmails, $sendDefaultDayEmails);
 
-      // Get the communities, and then the emails
+      // Do we have users to notify ?
       if ($usersToSendDigestsTo){
 
         $nbUsers = count($usersToSendDigestsTo);
 
-        $output->writeln(' # We have to notify ' . $nbUsers . ' user(s) today');
+        $output->writeln(' # We have to notify <comment>' . $nbUsers . ' user(s)</comment> today');
         $userCommunityRepository = $this->getContainer()->get('doctrine')->getRepository('metaUserBundle:UserCommunity');
 
         if (!$verbose) {
@@ -55,12 +56,15 @@ class digestSendCommand extends ContainerAwareCommand
           $progress->start($output, $nbUsers);
         }
 
+        // Initializes an array of messages
+        $messages = array();
+
         foreach ($usersToSendDigestsTo as $user) {
 
           if (!$verbose) {
             $progress->advance();
           } else {
-            $output->writeln("   - " . $user->getFullName());
+            $output->writeln("   - <question>" . $user->getFullName() . "</question>");
           }
 
           // Get userCommunity
@@ -68,23 +72,55 @@ class digestSendCommand extends ContainerAwareCommand
 
           if (!($user->getEnableSpecificEmails())){
           
-            // One mail for all the notifications
-            $nbNotifications = $this->getContainer()->get('logService')->countNotifications($user);
-            $notifications = $this->getContainer()->get('logService')->getNotifications($user);
-            if ($verbose) $output->writeln('     * ' . $nbNotifications . " aggregate notifications to send to " . $user->getEmail());
+            /*
+             * One mail for all the notifications
+             */
 
+            // -- DEBUG One mail for all the notifications
+            $nbNotifications = $this->getContainer()->get('logService')->countNotifications($user);
+            if ($verbose) $output->writeln('     * ' . $nbNotifications . " aggregate notification(s) to send to " . $user->getEmail());
+            // -- END DEBUG
+
+            $notificationsArray = $this->getContainer()->get('logService')->getNotifications($user);
+
+            $messages[] = \Swift_Message::newInstance()
+                ->setSubject($this->getContainer()->get('translator')->trans('user.digest.mail.subject'))
+                ->setFrom($this->getContainer()->getParameter('mailer_from'))
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->getContainer()->get('templating')->render(
+                        'metaGeneralBundle:Digest:digest.mail.html.twig',
+                        array('notifications' => $notificationsArray['notifications'], 'lastNotified' => $notificationsArray['lastNotified'], 'from' => $notificationsArray['from'], 'community' => null)
+                    ), 'text/html'
+                );
 
           } else {
             
-            // For each community, get HTML/Text to put in the mail
+            /*
+             * For each community, a separate mail
+             */
 
             foreach ($userCommunities as $userCommunity) {
               
               if ($userCommunity->getGuest()) continue;
 
+              // -- DEBUG One mail for all the notifications
               $nbNotifications = $this->getContainer()->get('logService')->countNotifications($user, $userCommunity->getCommunity());
-              $notifications = $this->getContainer()->get('logService')->getNotifications($user, null, $userCommunity->getCommunity());
-              if ($verbose) $output->writeln('     * ' . $userCommunity->getCommunity()->getName() . " : " . $nbNotifications . " notifications to send to " . $userCommunity->getEmail());
+              if ($verbose) $output->writeln('     * ' . $userCommunity->getCommunity()->getName() . " : " . $nbNotifications . " notification(s) to send to " . $userCommunity->getEmail());
+              // -- END DEBUG
+
+              $notificationsArray = $this->getContainer()->get('logService')->getNotifications($user, null, $userCommunity->getCommunity());
+              
+              $messages[] = \Swift_Message::newInstance()
+                ->setSubject($this->getContainer()->get('translator')->trans('user.digest.mail.subject'))
+                ->setFrom($this->getContainer()->getParameter('mailer_from'))
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->getContainer()->get('templating')->render(
+                        'metaGeneralBundle:Digest:digest.mail.html.twig',
+                        array('notifications' => $notificationsArray['notifications'], 'lastNotified' => $notificationsArray['lastNotified'], 'from' => $notificationsArray['from'], 'community' => $userCommunity->getCommunity())
+                    ), 'text/html'
+                );
 
             }
 
@@ -93,18 +129,16 @@ class digestSendCommand extends ContainerAwareCommand
           // We have the notifications, send the mail (or not)
           if ($sendMails && $nbNotifications > 0){
 
-            $message = \Swift_Message::newInstance()
-                ->setSubject($this->get('translator')->trans('user.invitation.mail.subject'))
-                ->setFrom($this->container->getParameter('mailer_from'))
-                ->setReplyTo($user->getEmail())
-                ->setTo($user->getEmail())
-                ->setBody(
-                    $this->renderView(
-                        'metaUserBundle:Mail:invite.mail.html.twig',
-                        array('user' => $authenticatedUser, 'inviteToken' => $token?$token->getToken():null, 'invitee' => ($user && !$user->isDeleted()), 'community' => $community, 'project' => null )
-                    ), 'text/html'
-                );
-            $this->get('mailer')->send($message);
+            $mailer = $container = $this->getContainer()->get('mailer');
+            $spool = $mailer->getTransport()->getSpool();
+            $transport = $this->getContainer()->get('swiftmailer.transport.real');
+
+            foreach ($messages as $message) {
+              $this->getContainer()->get('mailer')->send($message);
+            }
+
+            // Sends for real... maybe put that at the very end of the script ?
+            $spool->flushQueue($transport);
 
             if ($verbose) $output->writeln('     --> Mail sent');
 
