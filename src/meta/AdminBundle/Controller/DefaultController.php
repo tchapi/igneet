00a9a834b1d2d36400c6d419ddb6ea4cc68a6717
tchapi <regbasket@gmail.com>
2013-09-17 15:41:51 +0200

    Add more admin stuff

--- src/meta/AdminBundle/Controller/DefaultController.php
+++ src/meta/AdminBundle/Controller/DefaultController.php
@@ -4,10 +4,22 @@ namespace meta\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use meta\AdminBundle\Stats\Stats;
+
 class DefaultController extends Controller
 {
-    public function homeAction()
+    public function homeAction($start,$end)
     {
-        return $this->render('metaAdminBundle:Default:home.html.twig');
+
+        if (is_null($start) || is_null($end)){
+
+          $end = date('Y-m-d'); // now
+          $start = date('Y-m-d', strtotime("now - 7 days"));
+
+        }
+
+        $stats = $this->get("stats")->getCombinedStats($start,$end);
+
+        return $this->render('metaAdminBundle:Default:home.html.twig', array("stats" => $stats[0], "start_date" => $start, "end_date" => $end));
     }
 }
