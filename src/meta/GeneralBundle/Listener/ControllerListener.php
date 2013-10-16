a24e32af0bfb8d7c4f5c7ae8c90f19e1fc962fee
tchapi <regbasket@gmail.com>
2013-10-16 20:10:56 +0200

    Now preExecuting on subrequests as well

--- src/meta/GeneralBundle/Listener/ControllerListener.php
+++ src/meta/GeneralBundle/Listener/ControllerListener.php
@@ -16,36 +16,36 @@ class ControllerListener {
  
     public function onCoreController(FilterControllerEvent $event) {
 
-        if(HttpKernelInterface::MASTER_REQUEST == $event->getRequestType()) {
+        // if(HttpKernelInterface::MASTER_REQUEST == $event->getRequestType()) {
 
-            $_controller = $event->getController();
+        $_controller = $event->getController();
 
-            /*
-             * $controller passed can be either a class or a Closure. This is not usual in Symfony2 but it may happen.
-             * If it is a class, it comes in array format
-             */
-            if (!is_array($_controller)) {
-                return;
-            }
-
-            if(isset($_controller[0])) {
-                $controller = $_controller[0];
-                if(method_exists($controller, 'preExecute')) {
-
-                    $response = $controller->preExecute($event->getRequest());
-
-                    if ($response instanceof Response || $response instanceof RedirectResponse){
-                        /* SO DIRTYYYYYY */
-                        /* http://stackoverflow.com/a/16167953/1741150 */
-                        $event->setController(function() use ($response) {
-                            return $response;
-                        });
-                    }
-
-                }
-            }
-
+        /*
+         * $controller passed can be either a class or a Closure. This is not usual in Symfony2 but it may happen.
+         * If it is a class, it comes in array format
+         */
+        if (!is_array($_controller)) {
+            return;
         }
+
+        if(isset($_controller[0])) {
+            $controller = $_controller[0];
+            if(method_exists($controller, 'preExecute')) {
+
+                $response = $controller->preExecute($event->getRequest());
+
+                if ($response instanceof Response || $response instanceof RedirectResponse){
+                    /* SO DIRTYYYYYY */
+                    /* http://stackoverflow.com/a/16167953/1741150 */
+                    $event->setController(function() use ($response) {
+                        return $response;
+                    });
+                }
+
+            }
+        }
+
+        // }
 
     }
 }
