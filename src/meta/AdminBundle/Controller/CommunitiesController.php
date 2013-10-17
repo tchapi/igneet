938ec923f0116c9119318928e1b617cc815e4de0
tchapi <regbasket@gmail.com>
2013-10-17 13:01:45 +0200

    Working editable dates for communities

--- src/meta/AdminBundle/Controller/CommunitiesController.php
+++ src/meta/AdminBundle/Controller/CommunitiesController.php
@@ -3,7 +3,8 @@
 namespace meta\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller,
-    Symfony\Component\HttpFoundation\Request;
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response;
 
 use meta\AdminBundle\Stats\Stats;
 
@@ -18,4 +19,32 @@ class CommunitiesController extends Controller
         return $this->render('metaAdminBundle:Communities:list.html.twig', array("communities" => $communities));
 
     }
+
+    public function extendAction(Request $request)
+    {
+
+        $uid = $request->request->get('pk');
+        $value = $request->request->get('value');
+
+        // Translate to ID
+        $id = $this->container->get('uid')->fromUId($uid);
+        
+        $communitiesRepository = $this->getDoctrine()->getRepository('metaGeneralBundle:Community\Community');
+        $community = $communitiesRepository->findOneById($id);
+
+        if ($community && strtotime($value) != FALSE ){
+
+          $community->setValidUntil(new \DateTime($value));
+          $em = $this->getDoctrine()->getManager();
+          $em->flush();
+
+          return new Response();
+
+        } else {
+
+          return new Response("Community does not exist, or wrong date format", 400);
+        }
+
+    }
+
 }
