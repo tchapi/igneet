58312e4802979ae3b252ba9ed4092a62933e5a6c
tchapi <regbasket@gmail.com>
2013-12-02 11:58:22 +0100

    Improve image chosing But still broken ...

--- src/meta/UserBundle/Controller/DefaultController.php
+++ src/meta/UserBundle/Controller/DefaultController.php
@@ -491,6 +491,9 @@ class DefaultController extends Controller
                     );
                     $objectHasBeenModified = true;
                     break;
+                case 'file': // In this case, no file was passed to upload, so we just pass our way
+                    $needsRedirect = true;
+                    break;
                 case 'picture':
                     $preparedFilename = trim(__DIR__.'/../../../../web'.$request->request->get('value'));
                     
