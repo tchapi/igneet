1b75b4b4ed9dc614a61bba2e586bbcb5fd79ae97
tchapi <regbasket@gmail.com>
2014-03-08 12:19:12 +0100

    Fix warning on $options

--- src/meta/ProjectBundle/Entity/StandardProjectRepository.php
+++ src/meta/ProjectBundle/Entity/StandardProjectRepository.php
@@ -107,7 +107,7 @@ class StandardProjectRepository extends EntityRepository
             ->andWhere( $guestCriteria .'u = :user OR u2 = :user')
             ->setParameter('user', $options['user']);
 
-    if ( !is_null($options['statuses']) ) { // We have to filter status
+    if ( isset($options['statuses']) && !is_null($options['statuses']) ) { // We have to filter status
       $query->andWhere('sp.status IN (:statuses)')
             ->setParameter('statuses', $options['statuses']);
     }
