209b9c1ffa5a4541f6211891e1a312c89ceb723e
tchapi <regbasket@gmail.com>
2014-06-29 11:34:19 +0200

    WIP Improve search and results, Add highlights Refactor Elastica query

--- src/meta/GeneralBundle/Controller/SearchController.php
+++ src/meta/GeneralBundle/Controller/SearchController.php
@@ -21,11 +21,13 @@ class SearchController extends Controller
     /*
      * Allow to search the wide index
      */
-    public function searchAction(Request $request)
+    public function searchAction(Request $request, $type)
     {
 
       $term = $request->request->get('term');
+      $limit = $this->container->getParameter('search.limit');
 
+      $authenticatedUser = $this->getUser();
       // No term ? Redirect
       if ($term == "") {
 
@@ -33,87 +35,200 @@ class SearchController extends Controller
                     'warning',
                     $this->get('translator')->trans('search.no.term')
                 );
-
         return $this->redirect($this->generateUrl('g_home_community'));
       
       }
 
-      $finder = $this->container->get('fos_elastica.finder.igneet');
+      if ($type !== null) {
+        $finder = $this->container->get('fos_elastica.finder.igneet.' . $type);
+      } else {
+        $finder = $this->container->get('fos_elastica.finder.igneet');
+      }
 
-      // Returns a mixed array of any objects mapped
-      $results = $finder->find($term);
+
+      // Creates the query correctly with a bool
+      $boolQuery = new \Elastica\Query\Bool();
+
+      $queryString = new \Elastica\Query\QueryString();
+      $queryString->setDefaultField('_all');
+      $queryString->setQuery($term);
+      $boolQuery->addMust($queryString);
+
+      $query = new \Elastica\Query($boolQuery);
+
+      $query->setHighlight(array(
+          "fields" => array("*" => new \stdClass)
+      ));
+      $query->setSize($limit);
+
+      // Returns a mixed array of any objects mapped + highlights
+      $results = $finder->findHybrid($query);
 
       // Init some arrays for the JSON
       $projects = array();
       $ideas = array();
       $users = array();
       $resources = array();
+        $types = $this->container->getParameter('project.resource_types');
       $lists = array();
+      $listitems = array();
       $wikipages = array();
 
-      foreach($results as $result) {
+      // Get all the communities the user is in
+      $validCommunities = $this->getDoctrine()->getRepository('metaUserBundle:User')->findCommunitiesOfUser($authenticatedUser, false);
+      $validGuestCommunities = $this->getDoctrine()->getRepository('metaUserBundle:User')->findCommunitiesOfUser($authenticatedUser, true);
 
-        // TODO FIXME
-        // TODO Check if $authenticatedUser has the right to see the items
-        // TODO FIXME
+      // Define a test function to know if we can display the result
+      $test = function ($project) use ($validCommunities, $validGuestCommunities, $authenticatedUser) {
+        
+        if ($project->isDeleted()) { return false; }
+        //  project is in one of my communities AND (project not private OR (project private AND owner or participant )  )
+        //    OR 
+        //  project in guest communities AND (owner OR participant)
+        $privateSpace = is_null($project->getCommunity());
+        $inCommunity = in_array($project->getCommunity(), $validCommunities);
+        $inProject = $project->getOwners()->contains($authenticatedUser) || $project->getParticipants()->contains($authenticatedUser);
+        $publicProject = !$project->isPrivate();
+        $inGuestCommunity = in_array($project->getCommunity(), $validGuestCommunities);
 
+        // error_log('inCommunity : ' . ($inCommunity?"true":"false"));
+        // error_log('inProject : ' . ($inProject?"true":"false"));
+        // error_log('publicProject : ' . ($publicProject?"true":"false"));
+        // error_log('inGuestCommunity : ' . ($inGuestCommunity?"true":"false"));
+
+        $allowed = ($privateSpace && $inProject) || ($inCommunity && ($publicProject || $inProject)) || ($inGuestCommunity && $inProject);
+
+        return ($allowed === true);
+      };
+
+      $rawTotal = 0;
+      foreach($results as $rawResult) {
+
+        $rawTotal += 1;
+        $result = $rawResult->getTransformed();
+        $highlights = end($rawResult->getResult()->getHighlights()); // Gets the last item of the array
+        $highlight = strip_tags($highlights[0], '<em>');
+
+        /* Project */
+        /***********/
         if ($result instanceof StandardProject) {
-          $projects[] = array(
-            'path' => $this->generateUrl('p_show_project', array('uid' => $this->container->get('uid')->toUId($result->getId()))),
-            'picture' => $result->getPicture(),
-            'title' => $result->getName(),
-            'extra' => $result->getHeadline()
-          );
-        } elseif ($result instanceof Idea) {
-          $ideas[] = array(
-            'path' => $this->generateUrl('i_show_idea', array('uid' => $this->container->get('uid')->toUId($result->getId()))),
-            'picture' => $result->getPicture(),
-            'title' => $result->getName(),
-            'extra' => $result->getHeadline()
-          );
-        } elseif ($result instanceof User) {
-          $users[] = array(
-            'path' => $this->generateUrl('u_show_user_profile', array('username' => $result->getUsername())),
-            'picture' => $result->getAvatar(),
-            'title' => $result->getFullName(),
-            'extra' => $result->getHeadline()
-          );
+
+          // User in community, etc ..
+          if ($test($result)) {
+            $projects[] = array(
+              'path' => $this->generateUrl('p_show_project', array('uid' => $this->container->get('uid')->toUId($result->getId()))),
+              'picture' => $result->getPicture(),
+              'title' => $result->getName(),
+              'highlight' => $highlight
+            );
+          }
+          
+        /* Project derivatives */
+        /***********************/
         } elseif ($result instanceof WikiPage) {
-          $wikipages[] = array(
-            'path' => $this->generateUrl('p_show_project_wiki_show_page', array('uid' => $this->container->get('uid')->toUId($result->getWiki()->getProject()->getId()), 'page_uid' => $this->container->get('uid')->toUId($result->getId()))),
-            'picture' => $result->getWiki()->getProject()->getPicture(),
-            'title' => $result->getTitle(),
-            'extra' => $result->getWiki()->getProject()->getName()
-          );
+            
+          if ($test($result->getWiki()->getProject())) {
+            $wikipages[] = array(
+              'path' => $this->generateUrl('p_show_project_wiki_show_page', array('uid' => $this->container->get('uid')->toUId($result->getWiki()->getProject()->getId()), 'page_uid' => $this->container->get('uid')->toUId($result->getId()))),
+              'picture' => null,
+              'title' => $result->getTitle(),
+              'extra' => $result->getWiki()->getProject()->getName(),
+              'extra_picture' => $result->getWiki()->getProject()->getPicture(),
+              'highlight' => $highlight
+            );
+          }
+
         } elseif ($result instanceof Resource) {
-          $resources[] = array(
-            'path' => $this->generateUrl('p_show_project_resource', array('uid' => $this->container->get('uid')->toUId($result->getProject()->getId()), 'resource_uid' => $this->container->get('uid')->toUId($result->getId()))),
-            'picture' => $result->getProject()->getPicture(),
-            'title' => $result->getTitle(),
-            'extra' => $result->getProject()->getName()
-          );
+          
+          if ($test($result->getProject())) {
+            $resources[] = array(
+              'path' => $this->generateUrl('p_show_project_resource', array('uid' => $this->container->get('uid')->toUId($result->getProject()->getId()), 'resource_uid' => $this->container->get('uid')->toUId($result->getId()))),
+              'picture' => '/bundles/metageneral/img/icons/' . $types[$result->getType()]["icon"] . '.png',
+              'title' => $result->getTitle(),
+              'extra' => $result->getProject()->getName(),
+              'extra_picture' => $result->getProject()->getPicture(),
+              'highlight' => $highlight
+            );
+          }
+
         } elseif ($result instanceof CommonList) {
-          $lists[] = array(
-            'path' => $this->generateUrl('p_show_project_list', array('uid' => $this->container->get('uid')->toUId($result->getProject()->getId()), 'list_uid' => $this->container->get('uid')->toUId($result->getId()))),
-            'picture' => $result->getProject()->getPicture(),
-            'title' => $result->getName(),
-            'extra' => $result->getProject()->getName()
-          );
+          
+          if ($test($result->getProject())) {
+            $lists[] = array(
+              'path' => $this->generateUrl('p_show_project_list', array('uid' => $this->container->get('uid')->toUId($result->getProject()->getId()), 'list_uid' => $this->container->get('uid')->toUId($result->getId()))),
+              'picture' => null,
+              'title' => $result->getName(),
+              'extra' => $result->getProject()->getName(),
+              'extra_picture' => $result->getProject()->getPicture(),
+              'highlight' => $highlight
+            );
+          }
+
         } elseif ($result instanceof CommonListItem) {
-          $listitems[] = array(
-            'path' => $this->generateUrl('p_show_project_list', array('uid' => $this->container->get('uid')->toUId($result->getCommonList()->getProject()->getId()), 'list_uid' => $this->container->get('uid')->toUId($result->getCommonList()->getId()))),
-            'picture' => $result->getCommonList()->getProject()->getPicture(),
-            'title' => $result->getText(),
-            'extra' => $result->getCommonList()->getProject()->getName()
-          );
+          
+          if ($test($result->getCommonList()->getProject())) {
+            $listitems[] = array(
+              'path' => $this->generateUrl('p_show_project_list', array('uid' => $this->container->get('uid')->toUId($result->getCommonList()->getProject()->getId()), 'list_uid' => $this->container->get('uid')->toUId($result->getCommonList()->getId()))),
+              'picture' => null,
+              'title' => $result->getText(),
+              'extra' => $result->getCommonList()->getProject()->getName(),
+              'extra_picture' => $result->getCommonList()->getProject()->getPicture(),
+              'highlight' => $highlight
+            );
+          }
+
+        /*  Idea   */
+        /***********/
+        } elseif ($result instanceof Idea) {
+
+          if (in_array($result->getCommunity(),$validCommunities)) {
+            $ideas[] = array(
+              'path' => $this->generateUrl('i_show_idea', array('uid' => $this->container->get('uid')->toUId($result->getId()))),
+              'picture' => $result->getPicture(),
+              'title' => $result->getName(),
+              'highlight' => $highlight
+            );
+          }
+
+        /*   User  */
+        /***********/
+        } elseif ($result instanceof User) {
+
+          $valid = false;
+          foreach ($result->getUserCommunities() as $userCommunity) {
+            if (in_array($userCommunity->getCommunity(),$validCommunities)) {
+              $valid = true;
+              break;
+            }
+          }
+
+          if ($valid) {
+            $users[] = array(
+              'path' => $this->generateUrl('u_show_user_profile', array('username' => $result->getUsername())),
+              'picture' => $result->getAvatar(),
+              'title' => $result->getFullName(),
+              'highlight' => $highlight
+            );
+          }
+
         }
         
       }
 
-      $total = count($results);
+      // Displays a notice if we hit the limit ... ask the user to be more precise
+      if ($rawTotal >= $limit) {
+        $showNotice = true;
+      } else {
+        $showNotice = false;
+      }
+
+      $total = count($projects) + count($ideas) + count($users) + count($wikipages) + count($lists) + count($listitems) + count($resources);
 
       return $this->render('metaGeneralBundle:Search:results.html.twig', array(
+        'icons' => $this->container->getParameter('search.icons'),
+        'limit' => $this->container->getParameter('search.limit'),
         'total' => $total,
+        'showNotice' => $showNotice,
         'term' => $term,
         'data' => array(
           'projects' => $projects, 
@@ -121,6 +236,7 @@ class SearchController extends Controller
           'users' => $users,
           'wikipages' => $wikipages,
           'lists' => $lists, 
+          'listitems' => $listitems,
           'resources' => $resources)
         ));
 
